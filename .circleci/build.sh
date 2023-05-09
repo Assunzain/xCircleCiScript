47c864730b2e8352f10d1d13ce3c8284d0173f96
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-05-09 15:17:12 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b dicoba --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b XT --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/Assunzain/Clang-RageTC clang
 
 # Main
-KERNEL_NAME=GreenLight-Tes # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=GreenLight-KernelSU # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=GreenLight_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
