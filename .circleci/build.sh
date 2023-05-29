a9c48a01e930edab31aada07931eb4e88ac1f99b
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-05-30 01:31:42 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b GLKernel-non-oc --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b GLKernelOC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone -b clang-15 --depth=1 https://gitlab.com/LeCmnGend/proton-clang clang
 
 # Main
