ca3b59ea8152cb2498c817dcdcb6705eaf86c64b
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-10 21:46:40 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
 git clone -b GLKernelOC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
-git clone -b clang-17 --depth=1 https://gitlab.com/LeCmnGend/proton-clang clang
+git clone -b clang-16 --depth=1 https://gitlab.com/LeCmnGend/proton-clang clang
 
 # Main
 KERNEL_NAME=Greenlight.v2.3-OC-[EOL]-STABLE # IMPORTANT ! Declare your kernel name
