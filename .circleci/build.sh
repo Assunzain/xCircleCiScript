376f66915c32a43970eaff8e11d6022c57efdc3e
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-10 23:27:28 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Downloading few Dependecies . . ."
+echo "Downloading few Dependecies . . . ."
 git clone -b GLKernelOC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone -b main --depth=1 https://github.com/Assunzain/Clang-RageTC clang
 
