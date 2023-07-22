68b8f54dcce8d3dbb095d3c48df8a13fe4966aec
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-22 23:55:14 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . . ."
 git clone -b GLKernelSU --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
-git clone -b main --depth=1 https://github.com/Assunzain/Clang-RageTC clang
+git clone -b clang-16 --depth=1 https://gitlab.com/LeCmnGend/proton-clang clang
 
 # Main
 KERNEL_NAME=GreenLight.v1.0-OC-KSU-ALPHA # IMPORTANT ! Declare your kernel name
