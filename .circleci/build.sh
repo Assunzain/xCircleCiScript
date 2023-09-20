b5f4d844843dace8b47f0ade543e3d89ea42cb88
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-09-20 15:31:48 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . . ."
-git clone -b satu --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b lineage-20.0 --depth=1 https://github.com/Assunzain/kernel_mido/commits/lineage-20.0 X01AD
 git clone -b main --depth=1 https://github.com/Assunzain/Clang-RageTC clang
 
 # Main
 KERNEL_NAME=GreenLight # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
-DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+DEVICE_DEFCONFIG=mido_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
 CLANG_ROOTDIR=$(pwd)/clang # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=AssunZain # Change with your own name or else.
 export KBUILD_BUILD_HOST=GLKernel # Change with your own hostname.
