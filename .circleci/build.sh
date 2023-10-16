fb52147aed5581dfdcb67724cd4573fd50dfc35e
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-10-16 16:16:18 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@ git clone -b Perf{KSU} --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD 
 git clone -b main --depth=1 https://github.com/Assunzain/Clang-RageTC clang
 
 # Main
-KERNEL_NAME=KSU-UPDATE # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=KSU-UPDATE1 # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
