3014e978ba834c957ba82ce83cdd9ec312c1318d
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-12-11 23:20:22 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b GLKernelSU-OC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b GLKernelSU-non-oc --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang/ -b clang-15 clang
 
 # Main
-KERNEL_NAME=GreenLight-v2.6-OC-KSU-STABLE1 # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=GreenLight-v2.6-NoN-OC-KSU-STABLE1 # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=GreenLight_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
