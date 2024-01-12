bccb841e869b30274d684d0da624b39fc882efdf
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-01-12 19:40:45 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@ git clone -b 14 --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone https://gitlab.com/LeCmnGend/proton-clang -b clang-15 clang
 
 # Main
-KERNEL_NAME=Tes1 # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=Tes2 # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
