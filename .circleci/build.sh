12d158fdd7e64d2db19fc68067b8097610256fc9
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-05 00:21:51 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b 14- --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b  GLKernelSU-OC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c -b thirteen clang
 
 # Main
