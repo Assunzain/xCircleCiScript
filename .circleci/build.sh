5ff617fc6ab408b4c3cd899c99d9e5265dc38ab7
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-05 00:42:53 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
 git clone -b  GLKernelSU-OC --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
-git clone --depth=1 https://gitlab.com/inferno0230/clang-r487747c -b thirteen clang
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz
+mkdir clang
+tar zxvf clang-r498229b.tar.gz -C clang
 
 # Main
-KERNEL_NAME=Tes21 # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=Tes # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
