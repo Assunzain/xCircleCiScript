e2a81bb99198c16ecd0087e0446ab9ac3917670f
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-03-03 21:50:33 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@ git clone -b Perf{KSU} --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD 
 git clone https://github.com/kdrag0n/proton-clang -b master clang --depth=1
 
 # Main
-KERNEL_NAME=b # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=a # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
