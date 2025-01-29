f9ca5c62f1083a5fb139bd409a367ac8fbc96280
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-01-29 10:06:04 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,6 +2,7 @@
 echo "Downloading few Dependecies . . ."
 git clone -b GLKernel-non-oc --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
+cd X01AD
 wget https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh
 bash setup.sh --cleanup
 rm -rf setup.sh
